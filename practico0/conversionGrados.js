7674ca09f0179719ad161793ef417c5a91744426
Magui Ichida <magui_ichida@hotmail.com>
2023-05-20 12:37:52 -0300

    Agrego el archivo conversionGrados.js

---
+++ practico0/conversionGrados.js
@@ -0,0 +1,12 @@
+let fahrenheit;
+let celsius;
+
+//Ejercicio 3.a. ¿Cuántos grados C son 50 F?
+fahrenheit = 50;
+celsius = (fahrenheit - 32) * 5 / 9;
+console.log(celsius);
+
+//Ejercicio 3.b. ¿Cuántos grados C son 75 F?
+fahrenheit = 75;
+celsius = (fahrenheit - 32) * 5 / 9;
+console.log(celsius);
