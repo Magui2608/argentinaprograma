f6c442fbe6180ec91e9256553f04b1147bafe706
Magui Ichida <magui_ichida@hotmail.com>
2023-05-20 12:48:23 -0300

    Subiendo archivo operaciones.js

---
+++ operaciones.js
@@ -0,0 +1,26 @@
+let x = 20;
+let y = 4;
+let z = 12;
+
+console.log(x / y + 24);
+
+console.log(x + y);
+
+y = 8;
+
+console.log(x + y);
+
+y = x + 8;
+
+console.log(x + y);
+
+x = 21;
+y = x + 8;
+
+console.log(y);
+
+x = 20;
+y = 4;
+z = 2;
+
+console.log(z * (x + y));
