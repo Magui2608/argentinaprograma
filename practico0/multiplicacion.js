0dcccb85645623b9cc32671985a8df7a07233e58
Magui Ichida <magui_ichida@hotmail.com>
2023-05-20 12:36:13 -0300

    Subiendo carpeta practico0 con los archivos de resta.js y multiplicacion.js

---
+++ practico0/multiplicacion.js
@@ -0,0 +1,5 @@
+//Ejercicio 1.b ¿Qué valor devuelve su programa si los números son 2 *17 * 6?
+let num1 = 2;
+let num2 = 17;
+let num3 = 6;
+console.log(num1*num2*num3);
---
+++ practico0/resta.js
@@ -0,0 +1,6 @@
+//Ejercicio 1.a ¿Qué valor devuelve su programa si el minuendo es 10 y el sustraendo 2?
+let minuendo = 10;
+let sustraendo = 2;
+console.log(minuendo-sustraendo);
+
+
