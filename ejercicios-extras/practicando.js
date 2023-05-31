1806a172f35af73e4b35ef79bdc9daa097338f60
Magui Ichida <magui_ichida@hotmail.com>
2023-05-30 21:30:07 -0300

    Agregando archivo practicando.js

---
+++ ejercicios-extras/practicando.js
@@ -0,0 +1,32 @@
+let num1 = 30;
+let num2 = 5;
+
+let num3;
+
+// suma, resta y multiplicacion
+num3 = num1 + num2;
+console.log('Suma: '+num3);
+
+num3 = num1 - num2;
+console.log('Resta: '+num3);
+
+num3 = num1 * num2;
+console.log('Multiplicacion: '+num3);
+
+// division
+num1 = 31;
+
+num3 = num1 / num2;
+console.log('Division: '+num3);
+
+// sobrante
+num3 = num1 % num2;
+console.log('Sobrante de la division: '+num3);
+
+// potencia
+num1 = 6;
+num2 = 2;
+
+num3 = num1 ** num2;
+console.log('Potencia: '+num3);
+
