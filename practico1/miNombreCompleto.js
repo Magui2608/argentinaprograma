c8ced120b4fde7b0d4a7a635d1ccbf12a0f6ad97
Magui Ichida <magui_ichida@hotmail.com>
2023-06-06 21:12:29 -0300

    Subiendo el archivo de miNombreCompleto.js

--- practico1/miNombreCompleto.js
+++ practico1/miNombreCompleto.js
@@ -1,4 +1,20 @@
+//PARTE 1: VARIABLES - 1. Escribir un programa que:
 
+// 1.a. Defina una variable nombre.
+let nombre;
+
+//1.b. Guarde en la variable nombre tu nombre real.
+nombre = "Magda";
+
+//1.c. Defina y asigne, en un mismo paso, la variable apellido. El valor a asignar deberá ser tu apellido real.
+let apellido = "Ichida";
+
+//1.d. Repita el paso anterior, pero esta vez definiendo la variable edad y asignandole tu edad real.
+let edad = 40;
+
+/*1.e. Por último, imprime por consola un mensaje que respete el siguiente 
+formato: "Hola, [nombre] [apellido]. Tienes [edad] años."*/
+console.log("Hola, %s %s. Tienes %i años.", nombre, apellido, edad);
 
 
 
