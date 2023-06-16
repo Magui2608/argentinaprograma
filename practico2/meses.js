c4e664f7a3b4e9fe7ff5ebdb744442a371b27483
Magui Ichida <magui_ichida@hotmail.com>
2023-06-15 21:19:46 -0300

    Agregando el archivo meses.js

---
+++ practico2/meses.js
@@ -0,0 +1,49 @@
+/*2. Escriba un programa que solicite al usuario ingresar un número entero del 1 al
+12 representando los meses de un año; donde 1 es enero, 2 es febrero, 3 es
+marzo, etc. Deberá guardar esta información en alguna de las estructuras ya
+vistas, es decir, 1 = enero, 2 = febrero, 3 = marzo, etc para luego ser utilizada en
+el texto a imprimir. Su programa deberá ser guardado en un archivo meses.js y
+consistirá de imprimir por consola la cantidad de días que tiene el mes
+ingresado bajo el siguiente formato:
+"La cantidad de dias del mes de [mes] es [cantidadDeDias]"
+Por ejemplo: si el número ingresado es 1, su programa imprimirá “La cantidad
+de dias del mes de enero es 31”.
+NOTAS:
+● Nota que el valor de [mes] no es el numero ingresado si no su
+correspondiente almacenado en la estructura de datos elegida.
+● Para el alcance de este ejercicio, podemos pensar que febrero siempre
+tiene 28 días.*/
+
+const readlineSync = require('readline-sync');
+const mensaje = readlineSync.question('Ingrese un numero entero del 1 al 12:\n');
+let mes = ["", "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+let cantidadDeDias;
+
+// FORMA 1
+if (mensaje == 1 || mensaje == 3 || mensaje == 5 || mensaje == 7 || mensaje == 8 || mensaje == 10 || mensaje == 12) {
+    cantidadDeDias = 31;
+    console.log("La cantidad de dias del mes de " + mes[mensaje] + " es " + cantidadDeDias);
+} else if (mensaje == 2) {
+    cantidadDeDias = 28;
+    console.log("La cantidad de dias del mes de " + mes[mensaje] + " es " + cantidadDeDias);
+} else if (mensaje == 4 ||mensaje == 6 || mensaje == 9 || mensaje == 11) {
+    cantidadDeDias = 30;
+    console.log("La cantidad de dias del mes de " + mes[mensaje] + " es " + cantidadDeDias);
+}else {
+    console.log("El numero ingresado es incorrecto.");
+}
+
+
+
+// FORMA 2 - este codigo no contempla que hacer si se ingresa otro numero diferente al solicitado
+/*
+if (mensaje == 1 || mensaje == 3 || mensaje == 5 || mensaje == 7 || mensaje == 8 || mensaje == 10 || mensaje == 12) {
+    cantidadDeDias = 31;
+} else if (mensaje == 2) {
+    cantidadDeDias = 28;
+} else {
+    cantidadDeDias = 30;
+}
+console.log("La cantidad de dias del mes de " + mes[mensaje] + " es " + cantidadDeDias);
+*/
+
